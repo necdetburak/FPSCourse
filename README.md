e23f2422d73e3fb5e682687dcc2c4fc71baf1f07
necdetburak <burak.karvan14@gmail.com>
2022-03-06 23:09:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FPSCourse
+
+Developed with Unreal Engine 4
---
+++ Source/FPSCourse/FPSCourseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSCourseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSCOURSE_API AFPSCourseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
